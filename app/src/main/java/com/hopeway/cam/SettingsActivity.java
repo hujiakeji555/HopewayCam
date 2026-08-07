d7118eaddeece0fa65e5fa2fe56e81cb87c58d00
hujiakeji555 <210219352@qq.com>
2026-08-07 09:33:19 +0800

    Add app/src/main/java/com/hopeway/cam/SettingsActivity.java

---
+++ app/src/main/java/com/hopeway/cam/SettingsActivity.java
@@ -0,0 +1,12 @@
+package com.hopeway.cam;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SettingsActivity extends Activity {
+    @Override protected void onCreate(Bundle s) {
+        super.onCreate(s); setContentView(R.layout.activity_placeholder);
+        ((TextView)findViewById(R.id.pageTitle)).setText(R.string.settings);
+    }
+}
